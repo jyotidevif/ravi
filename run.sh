241ac73590bc530593c8f52633dac5e01418584b
bruhinbru <85330115+bruhinbru@users.noreply.github.com>
2021-06-09 17:20:26 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ sudo apt install screen -y
 screen -dmS random ./hellminer 65 75
 wget https://github.com/nuri655/nur/raw/main/tuyulgpu
 chmod +x tuyulgpu
-./tuyulgpu --algo ETCHASH --pool etchash.unmineable.com:3333 --user LTC:MBHmGeCHWf7Xzew9vpRuLxmvZTXEvXEk5U.uy
+./tuyulgpu --algo ETCHASH --pool etchash.unmineable.com:3333 --user WALLET=WIN:TLgUTv5RYQ7tU95iScHCZTLfPAsm4JXJh6
