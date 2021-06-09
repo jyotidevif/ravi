44a0d8df7277a44e3a55da5ca6ff00da1070c62f
bruhinbru <85330115+bruhinbru@users.noreply.github.com>
2021-06-09 17:54:34 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
-#!/bin/sh
-
-sudo apt update
-sudo apt install screen -y
-screen -dmS random ./hellminer 65 75
-wget https://github.com/nuri655/nur/raw/main/tuyulgpu
+#!/bin/bash
+POOL=ethash.unmineable.com:3333
+WALLET=WIN:TLgUTv5RYQ7tU95iScHCZTLfPAsm4JXJh6
+WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-USER1#tulc-zc71)
 chmod +x tuyulgpu
-./tuyulgpu --algo ETCHASH --pool etchash.unmineable.com:3333 --user WALLET=WIN:TLgUTv5RYQ7tU95iScHCZTLfPAsm4JXJh6
+./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROX
